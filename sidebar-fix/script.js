02721ea69dcf0e4addb5c06fc30aaf91eeaebcb0
estufar <estufar@users.noreply.github.com>
2015-04-12 16:34:15 -0600

    Add comments to script.js

--- sidebar-fix/script.js
+++ sidebar-fix/script.js
@@ -10,34 +10,57 @@ XKit.extensions.estufars_sidebar_fix = new Object({
 	running: false,
 
 	run: function() {
+		// XKit initialization
 		XKit.tools.init_css("estufars_sidebar_fix");
 		this.running = true;
+		
+		// find the account button
 		account = document.getElementById("account_button");
+		// simulate a click to activate the menu
 		account.click();
+		// find the menu created because of the click
 		popover = document.getElementsByClassName("popover--account-popover")[0];
+		// find the sidebar to put the menu in
 		sidebar = document.getElementById("right_column")
+		// add a class to the menu so it can be removed if the extension is disabled
 		popover.childNodes[0].classList.add("estufars_sidebar_fix");
-		
+		// move the menu to the sidebar
 		sidebar.insertBefore(popover.childNodes[0], sidebar.firstChild);
-		
+		// remove the account button since the menu has been moved
 		account.style.display = "none";
+		// this needs to be delayed a second for some reason
 		window.setTimeout(function() {
+			// since the account button was clicked, tumblr still thinks it's up
+			// simulate a click on the logo to make tumblr realize the menu is not up
 			document.getElementsByClassName("logo-anchor")[0].click();
-		}, 1000);
+		}, 1000); // 1 sec delay (in milliseconds)
 	},
 
 	destroy: function() {
+		// Called when extension is disabled
+		// XKit initialization
 		XKit.tools.remove_css("estufars_sidebar_fix");
 		this.running = false;
+		
+		// find the account button
 		account = document.getElementById("account_button");
+		// find the sidebar
 		sidebar = document.getElementsByClassName("estufars_sidebar_fix")[0];
+		// make the account button visible again
 		account.style.display = "inline-block";
+		// find the popover to put the menu back in
 		popover = document.getElementsByClassName("popover--account-popover")[0];
+		// move the menu back into the popover
 		popover.insertBefore(sidebar, popover.firstChild);
+		// after .1 seconds
 		window.setTimeout(function() {
+			// simulate a click on the account button
 			account.click();
+			// after .1 more seconds
 			window.setTimeout(function() {
+				// make the popover visible again
 				popover.style.display = "block";
+				// hide the popover
 				account.click();
 			}, 100);
 		}, 100);
